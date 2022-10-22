e7c993fbe5d3637faabf72cbee674c210b54516b
JordanYeomans <jordan@meqprobe.com>
2022-10-22 21:05:47 +1100

    Working moving spline + scatter plot. X & Y

--- test_spline_graph.py
+++ test_spline_graph.py
@@ -1,56 +1,39 @@
 # https://stackoverflow.com/questions/50439506/dragging-points-in-matplotlib-interactive-plot
 
-import matplotlib.animation as animation
-from matplotlib.widgets import Slider, Button
-import matplotlib as mpl
-from matplotlib import pyplot as plt
-import scipy.interpolate as inter
 import numpy as np
+import scipy.interpolate as inter
+
+from matplotlib import pyplot as plt
+from matplotlib.widgets import Button
 
 func = lambda x: 0.1 * x ** 2
 
 # get a list of points to fit a spline to as well
 N = 10
-xmin = 0
 xmax = 10
-x = np.linspace(xmin, xmax, N)
-
-# spline fit
-yvals = func(x)
-spline = inter.InterpolatedUnivariateSpline(x, yvals)
-
-# figure.subplot.right
-mpl.rcParams['figure.subplot.right'] = 0.8
+xvals = np.linspace(0, 10, N)
+yvals = func(xvals)
+spline = inter.InterpolatedUnivariateSpline(xvals, yvals)
 
 # set up a plot
 fig, axes = plt.subplots(1, 1, figsize=(9.0, 8.0), sharex=True)
+plt.subplots_adjust(right=0.8)
 ax1 = axes
 
 pind = None  # active point
 epsilon = 5  # max pixel distance
 
 
-def update(val):
-    global yvals
-    global spline
-    # update curve
-    for i in np.arange(N):
-        yvals[i] = sliders[i].val
-    l.set_ydata(yvals)
-    spline = inter.InterpolatedUnivariateSpline(x, yvals)
-    m.set_ydata(spline(X))
-    # redraw canvas while idle
-    fig.canvas.draw_idle()
-
-
 def reset(event):
+    global xvals
     global yvals
     global spline
     # reset the values
-    yvals = func(x)
-    for i in np.arange(N):
-        sliders[i].reset()
-    spline = inter.InterpolatedUnivariateSpline(x, yvals)
+    xvals = np.linspace(0, 10, N)
+    yvals = func(xvals)
+
+    spline = inter.InterpolatedUnivariateSpline(xvals, yvals)
+    l.set_xdata(xvals)
     l.set_ydata(yvals)
     m.set_ydata(spline(X))
     # redraw canvas while idle
@@ -64,7 +47,7 @@ def button_press_callback(event):
         return
     if event.button != 1:
         return
-    # print(pind)
+
     pind = get_ind_under_point(event)
 
 
@@ -79,13 +62,9 @@ def button_release_callback(event):
 def get_ind_under_point(event):
     'get the index of the vertex under point if within epsilon tolerance'
 
-    # display coords
-    # print('display x is: {0}; display y is: {1}'.format(event.x,event.y))
-    t = ax1.transData.inverted()
     tinv = ax1.transData
-    xy = t.transform([event.x, event.y])
-    # print('data x is: {0}; data y is: {1}'.format(xy[0],xy[1]))
-    xr = np.reshape(x, (np.shape(x)[0], 1))
+
+    xr = np.reshape(xvals, (np.shape(xvals)[0], 1))
     yr = np.reshape(yvals, (np.shape(yvals)[0], 1))
     xy_vals = np.append(xr, yr, 1)
     xyt = tinv.transform(xy_vals)
@@ -94,16 +73,15 @@ def get_ind_under_point(event):
     indseq, = np.nonzero(d == d.min())
     ind = indseq[0]
 
-    # print(d[ind])
     if d[ind] >= epsilon:
         ind = None
 
-    # print(ind)
     return ind
 
 
 def motion_notify_callback(event):
     'on mouse movement'
+    global xvals
     global yvals
     if pind is None:
         return
@@ -112,18 +90,21 @@ def motion_notify_callback(event):
     if event.button != 1:
         return
 
-    # update yvals
-    # print('motion x: {0}; y: {1}'.format(event.xdata,event.ydata))
+    xvals[pind] = event.xdata
     yvals[pind] = event.ydata
 
-    # update curve via sliders and draw
-    sliders[pind].set_val(yvals[pind])
+    l.set_ydata(yvals)
+    l.set_xdata(xvals)
+
+    spline = inter.InterpolatedUnivariateSpline(xvals, yvals)
+    m.set_ydata(spline(X))
+
     fig.canvas.draw_idle()
 
 
 X = np.arange(0, xmax + 1, 0.1)
 ax1.plot(X, func(X), 'k--', label='original')
-l, = ax1.plot(x, yvals, color='k', linestyle='none', marker='o', markersize=8)
+l, = ax1.plot(xvals, yvals, color='k', linestyle='none', marker='o', markersize=8)
 m, = ax1.plot(X, spline(X), 'r-', label='spline')
 
 ax1.set_yscale('linear')
@@ -135,17 +116,6 @@ ax1.grid(True)
 ax1.yaxis.grid(True, which='minor', linestyle='--')
 ax1.legend(loc=2, prop={'size': 22})
 
-sliders = []
-
-for i in np.arange(N):
-    axamp = plt.axes([0.84, 0.8 - (i * 0.05), 0.12, 0.02])
-    # Slider
-    s = Slider(axamp, 'p{0}'.format(i), 0, 10, valinit=yvals[i])
-    sliders.append(s)
-
-for i in np.arange(N):
-    # samp.on_changed(update_slider)
-    sliders[i].on_changed(update)
 
 axres = plt.axes([0.84, 0.8 - ((N) * 0.05), 0.12, 0.02])
 bres = Button(axres, 'Reset')
